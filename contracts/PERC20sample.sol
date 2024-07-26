8f178fba12673c795618ccf6c534ddc27befeca6
Rahmad <163022948+linndend@users.noreply.github.com>
2024-07-27 00:51:34 +0700

    PERC20sample.sol

--- contracts/PERC20sample.sol
+++ contracts/PERC20sample.sol
@@ -1,15 +1,32 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "./PERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract viinnsmoker is ERC721 {
-    constructor() ERC721("viinnsmoker", "VNS") {}
+    uint256 private _currentTokenId = 0;
+    mapping(address => uint256) private _balances;
 
-    
-     function mint() public {
-        _mint(msg.sender, 100*10**18);
+    event NFTMinted(address recipient, uint256 tokenId);
+
+    constructor() ERC721("viinnsmoker", "VNS") {}
+	
+    function mintNFT(address recipient) public returns (uint256) {
+        _currentTokenId += 1;
+        uint256 newItemId = _currentTokenId;
+        _mint(recipient, newItemId);
+        
+        emit NFTMinted(recipient, newItemId);  
+
+        return newItemId;
     }
+	
+    function burnNFT(uint256 tokenId) public {
+        _burn(tokenId);
+    }
+	
+	function balanceOf(address account) public view virtual override returns (uint256) {
+		require(msg.sender == account, "ERC721: msg.sender != account");
+		return _balances[account];
+	}
 }
